24346daf4c6d30b242127cae62df551b9044ed3a
nedhowley <ned@howley.com>
2019-04-26 01:01:53 +0100

    More

--- footer.php
+++ footer.php
@@ -9,15 +9,16 @@
  ?>
 
 <footer class="footer" role="contentinfo">
-	<p class="footer__copyright">&copy;
-		<?php echo date('Y') ?>
-		<?php bloginfo('name') ?>.
-	</p>
+	<div class="footer__inside">
+		<p class="footer__copyright">&copy;
+			<?php echo date('Y') ?>
+			<?php bloginfo('name') ?>.
+		</p>
 
-	<?php wp_nav_menu(array('theme_location' => 'footer-menu', 'items_wrap'=> '%3$s', 'container' => false)) ?>
+		<?php wp_nav_menu(array('theme_location' => 'footer-menu', 'items_wrap'=> '%3$s', 'container' => false)) ?>
 
-	<?php wp_nav_menu(array('theme_location' => 'social-menu', 'container' => false, 'menu_class' => 'footer__social')) ?>
-
+		<?php wp_nav_menu(array('theme_location' => 'social-menu', 'container' => false, 'menu_class' => 'footer__social')) ?>
+	</div>
 </footer>
 
 </div>
