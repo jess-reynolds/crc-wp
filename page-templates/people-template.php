7c20a17dce3ede8f6b20dfe0403f6ee205fab8e0
nedhowley <ned@howley.com>
2019-05-09 23:17:07 +0100

    df

--- page-templates/people-template.php
+++ page-templates/people-template.php
@@ -35,8 +35,6 @@ get_header(); if (have_posts()) : while (have_posts()) : the_post(); ?>
         <p class="prose">
             <?php echo get_post_meta($post->ID, "people_intro", true); ?>
         </p>
-        <img title="Home"
-            src="<?php bloginfo('template_url'); ?>/assets/images/icon-club.png" />
     </div>
 </div>
 
